a1c22e5f23a80207acf966ccc6837002e0041e5f
fatmaboodai <Fatmaboodai@hotmail.com>
2025-08-31 22:43:06 +0300

    added vercel analytics

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Analytics } from "@vercel/analytics/react";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
 import Navbar from "./components/Navbar";
@@ -50,6 +51,7 @@ const App = () => (
 	<QueryClientProvider client={queryClient}>
 		<LanguageProvider>
 			<AppContent />
+			<Analytics />
 		</LanguageProvider>
 	</QueryClientProvider>
 );
